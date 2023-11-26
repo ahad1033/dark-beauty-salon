c098041a24d3a39040c5acce784e602ef33b47b0
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-11-26 23:06:59 +0600

    service page color retouched

---
+++ src/Pages/ServicePage/AllServicesPrice.jsx
@@ -0,0 +1,355 @@
+import React from "react";
+
+const AllServicesPrice = () => {
+  return (
+    <div className="pt-8 pricing-5 pricing-section division">
+      <div className="container">
+        <div className="pricing-5-wrapper text-white">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="pricing-5-table left-column wow fadeInUp">
+                <div className="pricing-5-category mb-4">
+                  <h3 className="text-white">Cutting & Styling</h3>
+                </div>
+
+                <ul className="pricing-list">
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Women's Haircut</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$68 - $153</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Men's Haircut</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$45 - $92</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Blow Dry</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">From $44</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Updo/Style</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$75 - $164</p>
+                      </div>
+                    </div>
+
+                    <div className="price-txt">
+                      <p className="text-white">(shampoo not included)</p>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Child’s Haircut</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$32 - $68</p>
+                      </div>
+                    </div>
+
+                    <div className="price-txt">
+                      <p className="text-white">(12 years old + under)</p>
+                    </div>
+                  </li>
+                </ul>
+
+                <div className="pricing-5-category mt-4 mb-4">
+                  <h3 className="text-white">Hair Coloring</h3>
+                </div>
+
+                <ul className="pricing-list">
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Single Process</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$130 - $195</p>
+                      </div>
+                    </div>
+
+                    <div className="price-txt">
+                      <p className="text-white">(gloss not included)</p>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Double Process</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$290 - $375</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Full Head Highlights</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$345 - $380</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Half Head Highlights</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$260 - $290</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Balayage</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$85 - $220</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Color Refresh</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$95 - $130</p>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-6">
+              <div className="pricing-5-table right-column wow fadeInUp">
+                <div className="pricing-5-category p5-top mb-4">
+                  <h3 className="text-white">Hair Treatments</h3>
+                </div>
+
+                <ul className="pricing-list">
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Brazilian Blow Out</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$180 - $255</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Keratin Complex Express</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$210</p>
+                      </div>
+                    </div>
+
+                    <div className="price-txt">
+                      <p className="text-white">(Service length 60 minutes)</p>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Keratin Complex</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$280 - $420</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Keratin Complex Max</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$350 - $495</p>
+                      </div>
+                    </div>
+
+                    <div className="price-txt">
+                      <p className="text-white">
+                        (Includes KC MAX Treatment Spray)
+                      </p>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Permanent Wave</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$185 - $240</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Hair Gloss</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$40 - $55</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Safe Color Treatment</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$35 - $95</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Hair & Scalp Treatments</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$45 - $240</p>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+
+                <div className="pricing-5-category mt-4 mb-4">
+                  <h3 className="text-white">Hair Extensions</h3>
+                </div>
+
+                <ul className="pricing-list">
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Blowdry with Extensions</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$65 - $95</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Extensions Service</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$110 per hour</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Keratin Hair Extensions</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$720 - $860</p>
+                      </div>
+                    </div>
+                  </li>
+
+                  <li className="pricing-5-item">
+                    <div className="detail-price">
+                      <div className="price-name">
+                        <p className="text-white">Hair Extension Removal</p>
+                      </div>
+                      <div className="price-dots"></div>
+                      <div className="price-number">
+                        <p className="text-white">$85 - $275</p>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <div className="more-btn mt-6">
+              <a href="booking.html" className="btn btn--tra-white hover--gold">
+                Book Online
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllServicesPrice;
